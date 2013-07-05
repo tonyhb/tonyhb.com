688f4e891055ac19710235a7398cf42cfd748d32
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-07-05 22:16:25 +0100

    Adds a bash script to set dates of posts

---
+++ posts/dates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch -t 201307051146 "New Blog.md"
+touch -t 201307050430 "Does this really matter?.md"
+touch -t 201205181000 "Using Sphinx within Magento, plus optimising search result rankings, weights and relevancy.md"
