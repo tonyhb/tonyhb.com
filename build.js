c2491c473ec8d1bb6697c50c2dbd8336c3ee8b78
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-07-09 23:56:15 +0100

    Adds a build.js file for RequireJS optimization

---
+++ build.js
@@ -0,0 +1,15 @@
+({
+  baseUrl: "public/js",
+  name: "init",
+  mainConfigFile: "public/js/config.js",
+  paths: {
+    templates: "../templates",
+    modernizr: "libs/modernizr",
+    jquery: "libs/jquery-1.10.1",
+    backbone: "libs/backbone",
+    underscore: "libs/underscore",
+    mustache: "libs/mustache",
+    text: "libs/require-text"
+  },
+  out: "app-built.js"
+})
